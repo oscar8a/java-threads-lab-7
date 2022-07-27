2b6fb3081a7aca553024bf3905f44b60aa7bab05
Oscar 8A <oscarochoacerrato@gmail.com>
2022-07-27 15:59:28 -0400

    Implement Callable

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,22 @@
 import java.util.Scanner;
-import java.util.concurrent.Callable;
+import java.util.concurrent.*;
 
 public class Main {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        ExecutorService exe = Executors.newSingleThreadExecutor();
+
+        Future<Integer> integer = exe.submit(getCallableInteger());
+        exe.shutdown();
+        System.out.println("Getting integer from callable ==> " + integer.get());
+    }
     public static Callable<Integer> getCallableInteger() {
-        // your code here
+        return new Callable<Integer>() {
+            @Override
+            public Integer call() throws Exception {
+                Scanner scanner = new Scanner(System.in);
+                return scanner.nextInt();
+            }
+        };
     }
 }
